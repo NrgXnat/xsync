323c9bde55ecd8af964ff15204f38d22d222baf9
Mohana Ramaratnam <mohanakannan9@gmail.com>
2016-10-18 12:39:47 +0530

    XNATX-503: Added filter for subject assessor resources

--- src/main/java/org/nrg/xsync/local/ExperimentFilter.java
+++ src/main/java/org/nrg/xsync/local/ExperimentFilter.java
@@ -581,6 +581,23 @@ public class ExperimentFilter {
 		return;
 	}
 
+	/**
+	 * Filter experiment resources.
+	 *
+	 * @param exp
+	 *            the exp
+	 * @throws IndexOutOfBoundsException
+	 *             the index out of bounds exception
+	 * @throws FieldNotFoundException
+	 *             the field not found exception
+	 */
+	public void filterSubjectAssessorResources(XnatSubjectassessordata exp)
+			throws Exception {
+		SyncConfigurationXsiType session = projectSyncConfiguration.getSynchronizationConfiguration().getSubjectAssessor(exp.getXSIType());
+	    SyncConfigurationResource sessionResources = session.getResources();
+		while (findAndRemoveExperimentResources(exp, sessionResources))	;
+		return;
+	}
 	
 	
 	/**
@@ -821,7 +838,7 @@ private boolean findAndRemoveScanResources(XnatImagescandataI scan, SyncConfigur
 			XnatSubjectassessordataI orig)
 					throws Exception {
 		XnatSubjectassessordataI assess = (XnatSubjectassessordataI) correctIDandLabel((XnatSubjectdata)newSubject,(XnatSubjectassessordata)orig);
-		filterExperimentResources((XnatExperimentdata)assess);
+		filterSubjectAssessorResources((XnatSubjectassessordata)assess);
 		for (final XnatAbstractresourceI res : assess.getResources_resource()) {
 			//modifySubjectAssessorResource((XnatAbstractresource) res, origSubject, newSubject);
 			modifyExptResource((XnatAbstractresource) res, (XnatExperimentdata)orig);
