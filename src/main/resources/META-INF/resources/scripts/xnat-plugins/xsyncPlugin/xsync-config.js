693194c34f23d0c43d62284f040190e83c0d8190
Mark M. Florida <markflorida@wustl.edu>
2018-11-06 12:36:03 -0600

    Properly populate configuration dialog form when rendering.

--- src/main/resources/META-INF/resources/scripts/xnat-plugins/xsyncPlugin/xsync-config.js
+++ src/main/resources/META-INF/resources/scripts/xnat-plugins/xsyncPlugin/xsync-config.js
@@ -495,8 +495,7 @@ if (typeof XSYNC.credentialsconfig === 'undefined') {
 						if(validateEmailAddresses() && validateNoOfRetryDays())
 						{
 							// Only include visible fields and checkboxes, which are of type 'hidden'
-							var json = form2js($('#root').find(':input').filter(':visible, [type="hidden"]')
-									.toArray());
+							var json = form2js($('#root-panel').find(':input').filter(':visible, [type="hidden"]').toArray());
 
 							// Source project not on the form
 							json.source_project_id = XNAT.data.context.project;
@@ -531,9 +530,9 @@ if (typeof XSYNC.credentialsconfig === 'undefined') {
             return {
                 kind: 'panel.form',
                 title: 'XSync Configuration',
-                //load: XSYNC.xsyncconfig.configuration,
+                load: XSYNC.xsyncconfig.configuration,
                 refresh: "/xapi/xsync/setup/projects/" + XNAT.data.context.project,
-                action: "#",
+                action: "#!",
                 contents: {
                     message: aspera(),
                     enabled: enabled(),
