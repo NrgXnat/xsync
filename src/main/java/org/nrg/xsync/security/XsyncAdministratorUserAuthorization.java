5cd2a04610845e67b5fc4136b7e23de3128f1bd4
ian-xnatworks <ian@xnatworks.io>
2026-07-23 00:33:11 -0400

    PIXI-232 First pass of creating container manager roll.

---
+++ src/main/java/org/nrg/xsync/security/XsyncAdministratorUserAuthorization.java
@@ -0,0 +1,26 @@
+package org.nrg.xsync.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.nrg.xapi.authorization.AbstractXapiAuthorization;
+import org.aspectj.lang.JoinPoint;
+import org.nrg.xdat.security.helpers.AccessLevel;
+import org.nrg.xdat.security.helpers.Roles;
+import org.nrg.xft.security.UserI;
+import org.nrg.xsync.utils.XsyncUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class XsyncAdministratorUserAuthorization extends AbstractXapiAuthorization {
+    @Override
+    protected boolean checkImpl(AccessLevel accessLevel, JoinPoint joinPoint, UserI user, HttpServletRequest request) {
+        return Roles.checkRole(user, XsyncUtils.XSYNC_ADMINISTRATOR_ROLE);
+    }
+
+    @Override
+    protected boolean considerGuests() {
+        return false;
+    }
+}
